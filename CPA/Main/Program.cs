69ba64c5993fb2032405b01a4762d544f67d962b
oneStep77 <adrian92_pl@yahoo.com>
2014-11-07 15:50:56 +0200

    Login startup app

--- CPA/Main/Program.cs
+++ CPA/Main/Program.cs
@@ -17,7 +17,7 @@ namespace Main
             int x = 10;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
